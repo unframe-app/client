03be481174f98ee20e017749f673b961130b59ba
unframe <unframe@unframe.com>
2023-02-04 11:39:14 -0500

    ready for NPM, need target server and api key passing

---
+++ readme.md
@@ -0,0 +1,41 @@
+# @unframe/client
+<!-- 
+[![Version](https://img.shields.io/npm/v/@unframe/client.svg)](http://npm.im/@unframe/client)
+[![Downloads](https://img.shields.io/npm/dm/@unframe/client.svg)](http://npm-stat.com/charts.html?package=@unframe/client)
+[![Downloads](https://img.shields.io/npm/dt/@unframe/client.svg)](http://npm-stat.com/charts.html?package=@unframe/client)
+[![Apache-2.0 License](https://img.shields.io/npm/l/@unframe/client?style=flat-square)](http://opensource.org/licenses/Apache-2.0) -->
+
+<!-- [![npm package](https://nodei.co/npm/@unframe/client.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/@unframe/client/) -->
+
+> Unframe node client - node module to interact with Unbook and Unframe services.
+
+## Requirements
+<!-- * node >= 8.9.0
+* npm < 8.0.0 use **@unframe/client@1.***
+* npm >= 8.0.0 use **@unframe/client@2.*** -->
+
+## Installation
+Run: `npm install --save-dev @unframe/client` or `yarn add --dev @unframe/client`
+
+<!-- You can add `install_and_scan` script to the package.json file to install and transfer dependency information using one command `npm run install_and_scan`:
+
+```
+"scripts": {
+  "install_and_scan": "npm install && @unframe/client -k apiKey -p Project"
+},
+``` -->
+
+
+
+Usage
+=====
+
+The converter-client.ts is meant as an example client to guide development of your own client code. 
+
+There are a few example files that can be run:
+
+npm run example-1
+npm run example-2
+npm run example-3
+npm run example-4
+npm run example-5
--- src/converter-client.ts
+++ src/converter-client.ts
@@ -2,10 +2,13 @@
 import axios, { AxiosResponse } from 'axios';
 import fs from 'fs';
 import FormData from 'form-data';
+import { Console } from 'console';
 
 
-export const targetServerUnbook = 'http://localhost:7071/api/Unbook'; 
+export const targetServerUnbook = 'http://localhost:7071/api/Unbook';
 export const targetServerUnfield = 'http://localhost:7071/api/Unfield';
+export const root = 'src/examples/';
+
 
 
 export interface IFieldInfo {
@@ -27,7 +30,7 @@ export interface IFieldInfo {
 // a function that iterates each file with extension .copybook in the target folder and calls the unbook endpoint
 // then saves the result to a file with the same name but with extension .fi
 export const unbook = async (pathToCopybookFile: string) => {
-   
+
     if (!pathToCopybookFile.endsWith('.copybook')) {
         console.error('path must end with .copybook');
         return;
@@ -38,33 +41,28 @@ export const unbook = async (pathToCopybookFile: string) => {
 
     var formData = new FormData();
     // Pass file stream directly to form
-    formData.append('file', fileStream, <FormData.AppendOptions>{ filepath: pathToCopybookFile , filename: pathToCopybookFile, contentType: 'text/json' });
-    try {
-        const unbooked: AxiosResponse = await axios.post(targetServerUnbook,formData, {
-            headers: {
+    formData.append('file', fileStream, <FormData.AppendOptions>{ filepath: pathToCopybookFile, filename: pathToCopybookFile, contentType: 'text/json' });
+
+    const unbooked: AxiosResponse = await axios.post(targetServerUnbook, formData, {
+        headers: {
             'Content-Type': 'multipart/form-data'
-            }
-        });
-        console.log('got # of items: ', unbooked.data.length);
-        // write the result to a file with the same name but with extension .fi
-        const outputfile =  pathToCopybookFile.replace('.copybook', '-fieldinfo.json');
-        // first wipe the file contents if the file exists
-        if (fs.existsSync(pathToCopybookFile)) { fs.truncate(outputfile, 0, () => { console.log('file wiped: ', outputfile); }); }
+        }
+    });
+    console.log('got # of items: ', unbooked.data.length);
+    // write the result to a file with the same name but with extension .fi
+    const outputfile = pathToCopybookFile.replace('.copybook', '-fieldinfo.json');
+    // first wipe the file contents if the file exists
+    if (fs.existsSync(pathToCopybookFile)) { fs.truncate(outputfile, 0, () => { console.log('file wiped: ', outputfile); }); }
 
-        // open the file for writing
-        const writeStream = fs.createWriteStream(outputfile);
-        // write the result to the file
-        writeStream.write(JSON.stringify(unbooked.data));
-        // close the file
-        writeStream.close();
-        console.log('written to file: ', outputfile);
-    } catch (error) {
-        console.log('error', error);
-    }
+    // open the file for writing
+    const writeStream = fs.createWriteStream(outputfile);
+    // write the result to the file
+    writeStream.write(JSON.stringify(unbooked.data));
+    // close the file
+    writeStream.close();
+    console.log('written to file: ', outputfile);
+
 }
-
-
-
 
 export const unfield = async (pathToFieldInfoFile: string, pathToBinaryFile: string, pathToOutputFile: string) => {
 
@@ -82,7 +80,7 @@ export const unfield = async (pathToFieldInfoFile: string, pathToBinaryFile: str
     // first wipe the output file if it exists
     if (fs.existsSync(pathToOutputFile)) { fs.truncate(pathToOutputFile, 0, () => { console.log('file wiped: ', pathToOutputFile); }); }
 
-    
+
     // open the binary file for reading binary byte by byte
     const fd = fs.openSync(pathToBinaryFile, 'r');
     const fdOutput = fs.openSync(pathToOutputFile, 'w');
@@ -99,11 +97,11 @@ export const unfield = async (pathToFieldInfoFile: string, pathToBinaryFile: str
         const totalAvailableBytes = stats.size;
         let totalBytesRead = 0;
         let recordCount = 0;
-        
+
         while (totalAvailableBytes - totalBytesRead > 0) {
             // more records to read
             recordCount++;
-        
+
             // iterate the array of field info objects
             for (let index = 0; index < fieldInfoObject.length; index++) {
                 const fieldInfo: IFieldInfo = fieldInfoObject[index];
@@ -139,11 +137,13 @@ export const unfield = async (pathToFieldInfoFile: string, pathToBinaryFile: str
             console.log(`Completed Record: ${recordCount}`);
             // start a new line on the output
             fs.writeSync(fdOutput, '\n');
-            
+
         }
         console.log(`Completed all records: ${recordCount}`);
     } catch (error) {
         console.log('error', error);
+        throw error;
+
     }
     finally {
         fs.closeSync(fd);
@@ -155,23 +155,20 @@ export const unfield = async (pathToFieldInfoFile: string, pathToBinaryFile: str
 
 export const unframe = async () => {
 
-    const root = 'src/examples/';
+    // read the targetFile from the args
+    const targetFile = process.argv[2];
+    // verify the argument is valid
+    if (!targetFile) {
+        throw new Error("filename is a required argument!");
+    }
+    console.log(`Processing ${targetFile} from Args...`);
 
-    // unbook(root + 'zoned_100klines.copybook');
-    // unfield(root + 'zoned_100klines-fieldinfo.json', root + 'zoned_100klines.bin', root + 'zoned_100klines.csv');
-
-    // await unbook(root + 'reg1_reg50.copybook');
-    // await unfield(root + 'reg1_reg50-fieldinfo.json', root + 'reg1_reg50.bin', root + 'reg1_reg50.csv');
-
-    // await unbook(root + 'comp3_100klines.copybook');
-    // await unfield(root + 'comp3_100klines-fieldinfo.json', root + 'comp3_100klines.bin', root + 'comp3_100klines.csv');
-
-    // const l = await unbook(root + 'occurs_address.copybook');
-    // const l1 = await unfield(root + 'occurs_address-fieldinfo.json', root + 'occurs_address.bin', root + 'occurs_address.csv');
-
-    // unbook(root + 'zoned_fewlines.copybook');
-    unfield(root + 'zoned_fewlines-fieldinfo.json', root + 'zoned_fewlines.bin', root + 'zoned_fewlines.csv');
-
+    try {
+        await unbook(`${root}${targetFile}.copybook`);
+        await unfield(`${root}${targetFile}-fieldinfo.json`, `${root}${targetFile}.bin`, `${root}${targetFile}.csv`);
+    } catch (error) {
+        console.log('error', error);
+    }
 }
 
- unframe();
+unframe();
